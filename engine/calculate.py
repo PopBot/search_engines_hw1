a63e4d689cbcdf8a32d34e2b243ece3106bdf7cd
Jarod Wong <jarodjaw@gmail.com>
2023-01-28 19:48:36 -0800

    TG I got processing done. Now I need to move on to running the program and stuffl

--- engine/calculate.py
+++ engine/calculate.py
@@ -1,3 +1,30 @@
+from urllib import parse
+from engine.scraping import parse_base_url_path_parameters_query_more
+from engine.input_output import get_absolute_file_path_for_relative_file
+import json
+
+
+def convert_nested_dict_to_list(nested_dict: dict) -> list:
+    # Given nested dictionary, read in order from top down the keys, then add to a list where
+    # the first element is the key, and the second element is the value
+    # nested_dict is a dictionary
+    # returns a list
+    list_of_lists = []
+    for key, value in nested_dict.items():
+        list_of_lists.append([key, value])
+    return list_of_lists
+
+
+def fix_all_root_urls(root_urls: list[str]) -> list[str]:
+    # Given a list of root URLs, returns a list of fixed root URLs
+    # root_urls is a list of strings
+    # returns a list of strings
+    fixed_root_urls = []
+    for root_url in root_urls:
+        fixed_root_urls.append(parse_base_url_path_parameters_query_more(root_url))
+    return fixed_root_urls
+
+
 def calculate_difference(x, y):
     return x - y
 
@@ -16,66 +43,62 @@ def generate_diff(x: list[str], y: list[str]) -> list[int]:
     # x and y are lists of strings
     # returns a list of integers
     diff = []
+
     for i in range(len(x)):
         if x[i] in y:
             diff.append(y.index(x[i]) - i)
     return diff
 
 
+def get_results(google_results: list[str], yahoo_results: list[str]):
+    # Given two lists of strings, compare the indices of matching strings from x to y
+    google = fix_all_root_urls(google_results)
+    yahoo = fix_all_root_urls(yahoo_results)
+    diff = generate_diff(google, yahoo)
+    spearman_coefficient = 0
+    percent_overlap = 0
+    if len(diff) > 1:
+        squared_diffs = [d ** 2 for d in diff]
+        sum_squared_diffs = sum(squared_diffs)
+        numerator = 6 * sum_squared_diffs
+        denominator = calculate_denominator(len(diff))
+        percent_overlap = 100 * (len(diff) / len(google_results))  # Number of matching results / Number of results from Google
+        spearman_coefficient = 1 - (numerator / denominator)
+    elif len(diff) == 1:    # Just one matching result
+        spearman_coefficient = 1
+        percent_overlap = 100 * (len(diff) / len(google_results))  # Number of matching results / Number of results from Google
+
+    return len(diff), percent_overlap, spearman_coefficient
 
 
-
-#############
-
-import time
-
-
-def spearmans_rank_correlation_coefficient(x, y):
-    # x and y are lists of numbers of the same length
-    # returns the Spearman's rank correlation coefficient
-    # between x and y
-    # https://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient
-    x_rank = rank(x)
-    y_rank = rank(y)
-    return pearsons_correlation_coefficient(x_rank, y_rank)
-
-
-def rank(x):
-    # x is a list of numbers
-    # returns a list of ranks of the numbers in x
-    # https://en.wikipedia.org/wiki/Ranking
-    sorted_x = sorted(x)
-    rank = []
-    for i in range(len(x)):
-        rank.append(sorted_x.index(x[i]) + 1)
-    return rank
-
-
-def pearsons_correlation_coefficient(x, y):
-    # x and y are lists of numbers of the same length
-    # returns the Pearson's correlation coefficient
-    # between x and y
-    # https://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
-    n = len(x)
-    sum_x = sum(x)
-    sum_y = sum(y)
-    sum_x_squared = sum([i ** 2 for i in x])
-    sum_y_squared = sum([i ** 2 for i in y])
-    psum = sum([x[i] * y[i] for i in range(n)])
-    num = psum - (sum_x * sum_y / n)
-    den = ((sum_x_squared - sum_x ** 2 / n) * (sum_y_squared - sum_y ** 2 / n)) ** .5
-    if den == 0:
-        return 0
-    return num / den
+def process_queries():
+    index = 0
+    average_percentage_data = []
+    average_coefficient_data = []
+    average_count_matches_data = []
+    results_to_write = ['Queries, Number of Overlapping Results, Percent Overlap, Spearman Coefficient']
+    yahoo_results = get_absolute_file_path_for_relative_file("results.json")
+    google_results = get_absolute_file_path_for_relative_file("Google_Result2.json")
+    # Read the results from the files
+    yahoo_results_data = json.load(open(yahoo_results))
+    google_results_data = json.load(open(google_results))
+    for query in google_results_data.keys():
+        index += 1
+        google_data = google_results_data[query]
+        yahoo_data = yahoo_results_data[query]
+        count_matches, percent_overlap, spearman_coefficient = get_results(google_data, yahoo_data)
+        average_percentage_data.append(percent_overlap)
+        average_count_matches_data.append(count_matches)
+        average_coefficient_data.append(spearman_coefficient)
+        results_to_write.append(f"Query {index}, {count_matches}, {percent_overlap}, {spearman_coefficient}")
+    average_percentage = sum(average_percentage_data) / len(average_percentage_data)
+    average_count_matches = sum(average_count_matches_data) / len(average_count_matches_data)
+    average_coefficient = sum(average_coefficient_data) / len(average_coefficient_data)
+    results_to_write.append(f"Averages, {average_count_matches}, {average_percentage}, {average_coefficient}")
+    with open(get_absolute_file_path_for_relative_file("hw1.csv"), "w") as f:
+        f.write("\n".join(results_to_write))
 
 
-def calculate_spearman_rank_coefficient():
+def write_results_out_to_file():
+    yahoo_results = get_absolute_file_path_for_relative_file("results.json")
     pass
-
-
-
-
-
-
-
-YAHOO_SEARCH_URL = "https://search.yahoo.com/search?p="
